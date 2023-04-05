b676c2c63c262341b548ca6c279143e7227e4541
Ali Alhoshaiyan <ahoshaiyan@fastmail.com>
2023-04-05 04:24:56 +0300

    Add cleansing for data before validation

--- lib/mini_defender/validates_input.rb
+++ lib/mini_defender/validates_input.rb
@@ -4,9 +4,21 @@ module MiniDefender::ValidatesInput
   extend ActiveSupport::Concern
 
   def validate!(rules, coerced = false)
-    data = params.to_unsafe_hash.deep_stringify_keys
+    data = cleanse_data(params.to_unsafe_hash.deep_stringify_keys)
     validator = MiniDefender::Validator.new(rules, data)
     validator.validate!
     coerced ? validator.coerced : validator.data
+  end
+
+  private
+
+  def cleanse_data(data)
+    data = data.to_h do |k, v|
+      v = v.strip
+      v = nil if v.blank?
+      [k, v]
+    end
+
+    data.compact
   end
 end
