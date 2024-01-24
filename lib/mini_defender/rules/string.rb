4eb204bad893d23f9ba4c7d296f94450e8ce28ba
Ali Alhoshaiyan <ahoshaiyan@fastmail.com>
2024-01-24 23:11:48 +0300

    Add relaxed string rule

--- lib/mini_defender/rules/string.rb
+++ lib/mini_defender/rules/string.rb
@@ -1,12 +1,28 @@
 # frozen_string_literal: true
 
 class MiniDefender::Rules::String < MiniDefender::Rule
+  def initialize(mode = 'strict')
+    @mode = mode
+  end
+
   def self.signature
     'string'
   end
 
+  def coerce(value)
+    value.to_s
+  end
+
   def passes?(attribute, value, validator)
-    value.is_a?(String)
+    if @mode == 'relaxed'
+      value.is_a?(String) ||
+        value.is_a?(Integer) ||
+        value.is_a?(Float) ||
+        value.is_a?(FalseClass) ||
+        value.is_a?(TrueClass)
+    else
+      value.is_a?(String)
+    end
   end
 
   def message(attribute, value, validator)
