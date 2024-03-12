5ba2ec2588c5d8a1fa398b83386a7b5b3e61977d
Salman Altuwayjiri <alk-333@hotmail.com>
2024-03-12 22:50:44 +0300

    feat: allow negative integers in validation (#17)

--- test/rules/integer_test.rb
+++ test/rules/integer_test.rb
@@ -3,5 +3,39 @@
 require 'test_helper'
 
 class IntegerTest < Minitest::Test
+  def setup
+    @rule = MiniDefender::Rules::Integer.new
+  end
 
+  def test_passes_with_string_positive_integer
+    assert(@rule.passes?("test", "10", nil))
+  end
+
+  def test_passes_with_string_negative_integer
+    assert(@rule.passes?("test", "-10", nil))
+  end
+
+  def test_fails_with_float
+    refute(@rule.passes?("test", 10.5, nil))
+  end
+
+  def test_fails_with_string_float
+    refute(@rule.passes?("test", "10.5", nil))
+  end
+
+  def test_fails_with_non_numeric_string
+    refute(@rule.passes?("test", "not a number", nil))
+  end
+
+  def test_fails_with_nil
+    refute(@rule.passes?("test", nil, nil))
+  end
+
+  def test_fails_with_array
+    refute(@rule.passes?("test", [], nil))
+  end
+
+  def test_fails_with_hash
+    refute(@rule.passes?("test", {}, nil))
+  end
 end
