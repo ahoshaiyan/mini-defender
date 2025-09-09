8d551a15bc28df4af0b0350c31374fa6d783e618
Ali <ahoshaiyan@fastmail.com>
2025-09-09 12:00:27 +0300

    Improve Hostname Validation (#32)

--- lib/mini_defender/rules/hostname.rb
+++ lib/mini_defender/rules/hostname.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 
 class MiniDefender::Rules::Hostname < MiniDefender::Rule
-  HOSTNAME_REGEX = /\A[a-zA-Z0-9][a-zA-Z0-9\-]{1,63}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,63})+\.?\z/
+  HOSTNAME_REGEX = /\A([a-zA-Z0-9][a-zA-Z0-9\-]{,62})(\.[a-zA-Z0-9][a-zA-Z0-9\-]{0,62})+\.?\z/
 
   def self.signature
     'hostname'
   end
 
   def coerce(value)
-    value.to_s.downcase.strip
+    value.to_s
+      .downcase
+      .strip
+      .gsub(/.\z/, '')
   end
 
   def passes?(attribute, value, validator)
     value = value.to_s.downcase.strip
-    return false if value.length > 255
+    if value.length > 255
+      return false
+    end
 
     value.match?(HOSTNAME_REGEX)
   end
